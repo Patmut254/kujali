58f950d9a6b2264045a7455c5fd5fcd36672d626
Patmut254 <anitajehu13@gmail.com>
2025-11-29 18:39:54 +0300

    feat(handler): define generic ICommandHandler interface with execute method

--- libs/model/budgetting/notes/budget-notes/src/lib/domain/add-note.handler.ts
+++ libs/model/budgetting/notes/budget-notes/src/lib/domain/add-note.handler.ts
@@ -2,5 +2,9 @@ import { AddNoteToBudgetCommand } from './add-note.command';
 import { FunctionHandler } from '@ngfire/functions'; 
 import { getRepository } from '@ngfire/functions';
 
+export interface ICommandHandler<TCommand> {
+  execute(command: TCommand): Promise<void>;
+}
+
 
 export * from './add-note.command
